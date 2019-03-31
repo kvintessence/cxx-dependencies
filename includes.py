ae1aa42b0a64793950d3a345245bfc640cd87950
Denis Sorokin <deniss@zeptolab.com>
2019-03-31 19:18:05 +0300

    combined two scripts into one

--- includes.py
+++ includes.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 
 import argparse
 from source_files_finder import spawnSourceFiles
-from header_deps import recalculateDependencies, averageDependenciesCount
+from header_deps import recalculateDependencies, averageDependenciesCount, getDependencies
 from link_includes import linkIncludes
 from database import SourceFile
 from pony import orm as pony
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     parser.add_argument('-i', '--input', nargs='+', type=str, help="One or more files/dirs to be analyzed.")
     parser.add_argument('-r', '--repository', nargs='*', type=str, help="One or more files/dirs of code.")
     parser.add_argument('-sh', '--system-headers', dest='system_headers', action='store_true', help="Include system headers.")
+    parser.add_argument('-l', '--list', dest='list', action='store_true', help="Also print all included headers.")
     parser.set_defaults(system_headers=False)
     args = parser.parse_args()
 
@@ -25,12 +26,37 @@ if __name__ == "__main__":
     recalculateDependencies(allSourceFiles=True)
 
     print('')
+    print('Average dependencies count: %.1f' % averageDependenciesCount())
+    print('')
 
     with pony.db_session:
         sourceFiles = [sourceFile for sourceFile in SourceFile.select(lambda f: f.isTarget)]
         sourceFiles.sort(key=lambda sf: sf.dependenciesCount, reverse=True)
+
+        maxLength = max([len(sf.fullPath) for sf in sourceFiles])
+        tableLine = '-----+-' + '-' * (1 + maxLength)
+
+        if not args.list:
+            print(tableLine)
+
         for sourceFile in sourceFiles:
+            if args.list:
+                print(tableLine)
+
             print('%4d | %s' % (sourceFile.dependenciesCount, sourceFile.fullPath))
 
-    print('')
-    print('Average dependencies count: %.1f' % averageDependenciesCount())
+            if args.list:
+                print(tableLine)
+
+            dependencies = sorted(getDependencies(sourceFile))
+            dependencies.remove(sourceFile.fullPath)
+
+            if args.list and len(dependencies) > 0:
+                for dependency in dependencies:
+                    print('     | %s' % dependency)
+
+                print(tableLine)
+                print('')
+
+        if not args.list:
+            print(tableLine)
